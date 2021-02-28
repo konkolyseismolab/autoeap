8ba7ea663b6debd07f0a07b07fab727196b54633
astrobatty <astrobatty@gmail.com>
2021-02-28 19:36:08 +0100

    do not use tqdm notebook feature

--- src/autoeapcore.py
+++ src/autoeapcore.py
@@ -315,24 +315,11 @@ def aperture_prep(inputfile,campaign=None,show_plots=False,save_plots=False):
     """Loop over each image and detect stars for each of them separatly"""
     import os
 
-    def isnotebook():
-        try:
-            shell = get_ipython().__class__.__name__
-            if shell == 'ZMQInteractiveShell':
-                return True   # Jupyter notebook or qtconsole
-            elif shell == 'TerminalInteractiveShell':
-                return False  # Terminal running IPython
-            else:
-                return False  # Other type (?)
-        except NameError:
-            return False      # Probably standard Python interpreter
-
     from sklearn.cluster import DBSCAN
     import matplotlib.gridspec as gridspec
     from lightkurve.utils import LightkurveWarning
     import os
-    if isnotebook(): from tqdm.notebook import tqdm
-    else: from tqdm import tqdm
+    from tqdm import tqdm
 
     import autoeap.photutils_stable as photutils
 
